3215eaa508d399abf2132c3e721c989b2c9c7bec
drice0331 <drice@Davids-MacBook-Air-2.local>
2015-01-08 09:34:49 -0800

    more ch 26

--- PhotoGallery/src/com/example/photogallery/FlickrFetchr.java
+++ PhotoGallery/src/com/example/photogallery/FlickrFetchr.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
 
 import android.net.Uri;
 import android.util.Log;
@@ -20,6 +24,8 @@ public class FlickrFetchr {
 	private static final String PARAM_EXTRAS = "extras";
 	
 	private static final String EXTRA_SMALL_URL = "url_s";
+	
+	private static final String XML_PHOTO = "photo";
 	
 	byte[] getUrlBytes(String urlSpec) throws IOException {
 		URL url = new URL(urlSpec);
@@ -61,6 +67,26 @@ public class FlickrFetchr {
 			Log.i(TAG, "Received xml: " + xmlString);
 		} catch (IOException e) {
 			Log.e(TAG, "Failed to fetch items", e);
+		}
+	}
+	
+	void parseItems(ArrayList<GalleryItem> items, XmlPullParser parser) throws XmlPullParserException, IOException {
+		int eventType = parser.next();
+		
+		while(eventType != XmlPullParser.END_DOCUMENT) {
+			if(eventType == XmlPullParser.START_TAG && XML_PHOTO.equals(parser.getName())) {
+				String id = parser.getAttributeValue(null, "id");
+				String caption = parser.getAttributeValue(null, "title");
+				String smallUrl = parser.getAttributeValue(null, EXTRA_SMALL_URL);
+				
+				GalleryItem item = new GalleryItem();
+				item.setId(id);
+				item.setCaption(caption);
+				item.setUrl(smallUrl);
+				items.add(item);
+			}
+			
+			eventType = parser.next();
 		}
 	}
 }
